a606b09de7391adf65bdf946e064be6733b275b0
Andrew Champion <awchampion@gmail.com>
2018-05-23 15:01:53 -0700

    add tests for primary TextTeaser classes

---
+++ tests/test_parser.py
@@ -0,0 +1,220 @@
+from textteaser.parser import Parser
+from typing import Dict, List, Tuple
+from .sample import Sample
+from .assert_ex import assert_ex
+
+
+class TestParser:
+    def _get_sample_keyword_data(self, samp):
+        return (samp.d['keywords'], samp.d['instances'])
+
+    def _get_keyword_result(self, text) -> List[Dict]:
+        parser = Parser()
+        return parser.getKeywords(text)
+
+    def _test_getKeywords(self, sample_name):
+        samp = Sample(sample_name)
+        text = samp.d['text']
+
+        (expect_kws, expect_insts) = self._get_sample_keyword_data(samp)
+        (result_kws, result_insts) = self._get_keyword_result(text)
+
+        assert_ex(
+            'total keyword count',
+            result_insts,
+            expect_insts)
+
+        assert_ex(
+            'unique keyword count',
+            len(result_kws),
+            len(expect_kws))
+
+        unexpected = [
+            kw
+            for kw in result_kws
+            if kw['word'] not in expect_kws.keys()]
+
+        assert_ex(
+            'keyword present', unexpected, [], test=len(unexpected) == 0)
+
+        miscounted = [
+            kw
+            for kw in result_kws
+            if kw['count'] != expect_kws[kw['word']]]
+
+        # list(filter(
+        #     lambda kw: kw not in unexpected and
+        #     kw['count'] != expect_kws[kw['word']],
+        #     result_kws))
+        assert_ex(
+            'keyword count', miscounted, [], test=len(miscounted) == 0)
+
+    def test_getKeywords_empty(self):
+        self._test_getKeywords('empty')
+
+    def test_getKeywords_essay(self):
+        self._test_getKeywords('essay-snark')
+
+    def _test_getSentenceLengthScore(self, sample_name):
+        parser = Parser()
+        samp = Sample(sample_name)
+        words = samp.d['compareWords']
+
+        expected = samp.d['lengthScore']
+        result = parser.getSentenceLengthScore(words)
+
+        assert_ex(
+            'sentence score',
+            result,
+            expected,
+            hint=' '.join(words))
+
+    def test_getSentenceLengthScore_empty(self):
+        self._test_getSentenceLengthScore('empty')
+
+    def test_getSentenceLengthScore_qtr_ideal(self):
+        self._test_getSentenceLengthScore('sentence-short')
+
+    def test_getSentenceLengthScore_half_ideal(self):
+        self._test_getSentenceLengthScore('sentence-medium')
+
+    def test_getSentenceLengthScore_full_ideal(self):
+        self._test_getSentenceLengthScore('sentence-ideal')
+
+    def test_getSentenceLengthScore_over_ideal(self):
+        self._test_getSentenceLengthScore('sentence-overlong')
+
+    def _test_getSentencePositionScore(self, pos, sentence_count, expected):
+        parser = Parser()
+        result = parser.getSentencePositionScore(pos, sentence_count)
+
+        assert_ex(
+            'sentence position score',
+            result,
+            expected,
+            hint='/'.join([str(pos), str(sentence_count)]))
+
+    def test_getSentencePositionScore_first(self):
+        self._test_getSentencePositionScore(0, 10, .17)
+
+    def test_getSentencePositionScore_second(self):
+        self._test_getSentencePositionScore(1, 10, .23)
+
+    def test_getSentencePositionScore_last(self):
+        self._test_getSentencePositionScore(9, 10, .15)
+
+    def _test_getTitleScore(self, sample_name):
+        samp = Sample(sample_name)
+        parser = Parser()
+        title = samp.d['compareTitle']
+        sentence = samp.d['compareWords']
+
+        expected = samp.d['titleScore']
+        result = parser.getTitleScore(title, sentence)
+
+        assert_ex(
+            'title score',
+            result,
+            expected,
+            hint=[title, sentence])
+
+    def test_getTitleScore_poor(self):
+        self._test_getTitleScore('sentence-1word')
+
+    def test_getTitleScore_short(self):
+        self._test_getTitleScore('sentence-short')
+
+    def test_getTitleScore_medium(self):
+        self._test_getTitleScore('sentence-medium')
+
+    def test_getTitleScore_ideal(self):
+        self._test_getTitleScore('sentence-ideal')
+
+    def test_getTitleScore_overlong(self):
+        self._test_getTitleScore('sentence-overlong')
+
+    def _test_splitSentences(self, sample_name):
+        samp = Sample(sample_name)
+        parser = Parser()
+
+        expected = samp.d['splitSentences']
+        result = parser.splitSentences(samp.d['text'])
+
+        assert_ex(
+            'sentence split',
+            result,
+            expected)
+
+    def test_splitSentences_single(self):
+        self._test_splitSentences('sentence-short')
+
+    def test_splitSentences_multi(self):
+        self._test_splitSentences('sentence-list')
+
+    def _test_splitWords(self, sample_name):
+        samp = Sample(sample_name)
+        parser = Parser()
+        text = samp.d['text']
+
+        expected = samp.d['splitWords']
+        result = parser.splitWords(text)
+
+        assert_ex(
+            'word split',
+            expected,
+            result)
+
+    def test_splitWords_empty(self):
+        self._test_splitWords('empty')
+
+    def test_splitWords_single(self):
+        self._test_splitWords('sentence-1word')
+
+    def test_splitWords_multi(self):
+        self._test_splitWords('sentence-medium')
+
+    def _test_removePunctations(self, sample_name):
+        parser = Parser()
+        samp = Sample(sample_name)
+
+        expected = samp.d['removePunctations']
+        result = parser.removePunctations(samp.d['text'])
+
+        assert_ex(
+            'punctuation removal',
+            result,
+            expected)
+
+    def test_removePunctations_empty(self):
+        self._test_removePunctations('empty')
+
+    def test_removePunctations_single(self):
+        self._test_removePunctations('sentence-1word')
+
+    def test_removePunctations_multi(self):
+        self._test_removePunctations('sentence-list')
+
+    def _test_removeStopWords(self, sample_name):
+        parser = Parser()
+        samp = Sample(sample_name)
+        words = parser.splitWords(samp.d['text'])
+
+        expected = samp.d['removeStopWords']
+        result = parser.removeStopWords(words)
+
+        assert_ex(
+            'remove stop words',
+            result,
+            expected)
+
+    def test_removeStopWords_empty(self):
+        self._test_removeStopWords('empty')
+
+    def test_removeStopWords_single(self):
+        self._test_removeStopWords('sentence-1word')
+
+    def test_removeStopWords_simple(self):
+        self._test_removeStopWords('sentence-2words')
+
+    def test_removeStopWords_multi(self):
+        self._test_removeStopWords('sentence-list')
---
+++ tests/test_summarizer.py
@@ -0,0 +1,245 @@
+""" Test class for Summarizer """
+from textteaser.summarizer import Summarizer
+from typing import Dict, List, Tuple
+from .sample import Sample
+from math import floor
+from random import shuffle
+import os.path as path
+from .assert_ex import assert_ex
+
+
+class TestSummarizer:
+    def compareFloat(self, val1, val2):
+        return (val1 * 100000 - val2 * 100000 < 2)
+
+    def randomize_list(self, src: List) -> List[any]:
+        dupe = list(src)
+        while dupe == src:
+            shuffle(dupe)
+
+        return dupe
+
+    def getTopKeywords(self, keywords):
+        return keywords[:10]
+
+    def getKeywordList(self, keywords):
+        return [x['word'] for x in keywords]
+
+    def _test_summarize(self, sample_name: str):
+        samp = Sample(sample_name)
+        summ = Summarizer()
+
+        expected = sorted(samp.d['sentences'], key=lambda x: x['rank'])
+        results = summ.summarize(samp.d['text'], samp.d['title'], None, None)
+
+        assert_ex(
+            'summary result count',
+            len(results),
+            len(expected),
+            hint=sample_name)
+
+        for rank, result in enumerate(results):
+            assert_ex(
+                'sentence text at rank',
+                result['sentence'],
+                expected[rank]['text'],
+                hint=[rank, result['sentence']])
+
+            assert_ex(
+                'sentence order at rank',
+                result['order'],
+                expected[rank]['order'],
+                hint=[rank, result['sentence']])
+
+    def test_summarize_cambodia(self):
+        self._test_summarize('cambodia')
+
+    def test_summarize_cameroon(self):
+        self._test_summarize('cameroon')
+
+    def test_summarize_canada(self):
+        self._test_summarize('canada')
+
+    def _test_getTopKeywords(self, sample_name: str):
+        samp = Sample(sample_name)
+        summ = Summarizer()
+
+        keywords = samp.d['keywords']
+        wordCount = samp.d['wordCount']
+        source = None
+        category = None
+
+        expected = samp.d['keywords']
+        results = summ.getTopKeywords(keywords, wordCount, source, category)
+
+        all_keywords = [kw['word'] for kw in keywords]
+        assert_ex(
+            'result count',
+            len(results),
+            len(expected))
+
+        for result in results:
+            try:
+                idx = all_keywords.index(result['word'])
+
+                assert_ex(
+                    'keyword count',
+                    result['count'],
+                    keywords[idx]['count'])
+
+                assert_ex(
+                    'keyword score',
+                    result['totalScore'],
+                    keywords[idx]['totalScore'])
+
+            except ValueError:
+                assert False, 'keyword error'
+
+    def test_getTopKeywords_cambodia(self):
+        self._test_getTopKeywords('cambodia')
+
+    def test_getTopKeywords_cameroon(self):
+        self._test_getTopKeywords('cameroon')
+
+    def test_getTopKeywords_canada(self):
+        self._test_getTopKeywords('canada')
+
+    def _test_sortScore(self, sample_name: str):
+        samp = Sample(sample_name)
+        summ = Summarizer()
+        sentences = self.randomize_list(samp.d['sentences'])
+
+        results = summ.sortScore(sentences)
+
+        for result in results:
+            try:
+                expected = [
+                    x for x in sentences
+                    if x['order'] == result['order']][0]
+
+                rv = result['totalScore']
+                ev = expected['totalScore']
+                cv = self.compareFloat(rv, ev)
+
+                assert_ex('sort score', rv, ev, test=cv)
+
+            except IndexError:
+                assert False, 'bad sentence list'
+
+    def test_sortScore_cambodia(self):
+        self._test_sortScore('cambodia')
+
+    def test_sortScore_cameroon(self):
+        self._test_sortScore('cameroon')
+
+    def test_sortScore_canada(self):
+        self._test_sortScore('canada')
+
+    def _test_sortSentences(self, sample_name: str):
+        samp = Sample(sample_name)
+        summ = Summarizer()
+        sentences = self.randomize_list(samp.d['sentences'])
+
+        results = summ.sortSentences(sentences)
+
+        for expected, result in enumerate(results):
+            assert_ex('sort order', result['order'], expected)
+
+    def test_sortSentences_cambodia(self):
+        self._test_sortSentences('cambodia')
+
+    def test_sortSentences_cameroon(self):
+        self._test_sortSentences('cameroon')
+
+    def test_sortSentences_canada(self):
+        self._test_sortSentences('canada')
+
+    def _test_computeScore(self, sample_name: str):
+        samp = Sample(sample_name)
+        summ = Summarizer()
+        sentences = [x['text'] for x in samp.d['sentences']]
+        titleWords = samp.d['titleWords']
+        topKeywords = self.getTopKeywords(samp.d['keywords'])
+
+        results = summ.computeScore(sentences, titleWords, topKeywords)
+
+        for order, result in enumerate(results):
+            expected = samp.d['sentences'][order]
+
+            assert_ex(
+                'sentence order',
+                result['order'],
+                order,
+                hint=[result['sentence'], expected['text']])
+
+            assert_ex(
+                'sentence score',
+                result['totalScore'],
+                expected['totalScore'],
+                test=self.compareFloat(
+                    result['totalScore'], expected['totalScore']),
+                hint=[result['sentence'], expected['text']])
+
+            assert_ex(
+                'sentence text',
+                result['sentence'],
+                expected['text'],
+                hint=[result['sentence'], expected['text']])
+
+    def test_computeScore_cambodia(self):
+        self._test_computeScore('cambodia')
+
+    def test_computeScore_cameroon(self):
+        self._test_computeScore('cameroon')
+
+    def test_computeScore_canada(self):
+        self._test_computeScore('canada')
+
+    def _test_sentence_scoring(self, sample_name, score_type):
+        samp = Sample(sample_name)
+        summ = Summarizer()
+
+        for sentence in samp.d['sentences']:
+            unpunct = summ.parser.removePunctations(sentence)
+            words = summ.parser.splitWords(unpunct)
+            topKeywords = self.getTopKeywords(samp.d['keywords'])
+            keywordList = self.getKeywordList(samp.d['keywords'])
+
+            expected = sentence[score_type]
+
+            if score_type == 'sbs':
+                result = summ.sbs(words, topKeywords, keywordList)
+
+            if score_type == 'dbs':
+                result = summ.dbs(words, topKeywords, keywordList)
+
+            assert_ex(
+                score_type,
+                expected,
+                result,
+                test=self.compareFloat(result, expected),
+                hint=unpunct)
+
+    def _test_sbs(self, sample_name: str):
+        self._test_sentence_scoring(sample_name, 'sbs')
+
+    def test_sbs_cambodia(self):
+        self._test_sbs('cambodia')
+
+    def test_sbs_cameroon(self):
+        self._test_sbs('cameroon')
+
+    def test_sbs_canada(self):
+        self._test_sbs('canada')
+
+    def _test_dbs(self, sample_name: str):
+        self._test_sentence_scoring(sample_name, 'dbs')
+
+    def test_dbs_cambodia(self):
+        self._test_dbs('cambodia')
+
+    def test_dbs_cameroon(self):
+        self._test_dbs('cameroon')
+
+    def test_dbs_canada(self):
+        self._test_dbs('canada')
