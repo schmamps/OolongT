639c9d794f7a7e693781feef35cb573c89dd6ffd
Andrew Champion <awchampion@gmail.com>
2018-05-30 13:37:09 -0700

    sorted imports update for PEP-8 member naming add _snip method for abbreviated text clarify params for sentence text (text) clarify params for top keyword list (top_keyword_list) iterate instead of discrete tests

--- tests/test_summarizer.py
+++ tests/test_summarizer.py
@@ -1,17 +1,22 @@
 """ Test class for Summarizer """
+from decimal import Decimal
+from io import open as io_open
 from math import floor
 from pathlib import Path
 from random import shuffle
-from textteaser.summarizer import Summarizer
+
 from textteaser.nodash import pluck
+from textteaser.summarizer import Summarizer
+
 from .assert_ex import assert_ex
 from .sample import Sample
 
 DATA_PATH = Path(__file__).parent.joinpath('data')
+SAMPLES = ['cambodia', 'cameroon', 'canada']
 
 
 class TestSummarizer:
-    def _compareFloat(self, val1, val2):
+    def _compare_float(self, val1, val2):
         """Compare two floating point values
 
         Arguments:
@@ -21,7 +26,7 @@ class TestSummarizer:
         Returns:
             bool -- the two values are close enough
         """
-        return (int(val1 * 100000) - int(val2 * 100000) < 2)
+        return (abs(val1 * 100000 - val2 * 100000) < 2.0)
 
     def _randomize_list(self, src):
         """Reorder a copy of the supplied list
@@ -39,7 +44,7 @@ class TestSummarizer:
         return dupe
 
     def _get_top_keywords(self, keywords):
-        """Shadow of Summarizer.getTopKeywords
+        """Shadow of Summarizer.get_top_keywords
 
         keywords should be pre-sorted by frequency
 
@@ -62,45 +67,49 @@ class TestSummarizer:
         """
         return [x['word'] for x in keywords]
 
-    def summarize(self, sample_name):
-        """Test Summarizer.summarize() with data from the selected sample
+    def _snip(self, val, max_len=20, separator=' ', ellip="..."):
+        text = val
+
+        if isinstance(val, list):
+            text = separator.join(val)
+
+        if len(text) <= max_len:
+            return text
+
+        return text[:max_len-len(ellip)] + ellip
+
+    def test_summarize(self):
+        """Test Summarizer.summarize() with data from the samples
 
         Arguments:
             sample_name {str} -- name of data source
         """
-        samp = Sample(DATA_PATH, sample_name)
-        summ = Summarizer()
+        for sample_name in SAMPLES:
+            samp = Sample(DATA_PATH, sample_name)
+            summ = Summarizer()
 
-        expected = sorted(samp.d['sentences'], key=lambda x: x['rank'])
-        results = summ.summarize(samp.d['text'], samp.d['title'], None, None)
-
-        assert_ex(
-            'summary result count',
-            len(results),
-            len(expected),
-            hint=sample_name)
-
-        for rank, result in enumerate(results):
-            assert_ex(
-                'sentence text at rank',
-                result['sentence'],
-                expected[rank]['text'],
-                hint=[rank, result['sentence']])
+            expected = sorted(samp.d['sentences'], key=lambda x: x['rank'])
+            results = summ.summarize(
+                samp.d['text'], samp.d['title'], None, None)
 
             assert_ex(
-                'sentence order at rank',
-                result['order'],
-                expected[rank]['order'],
-                hint=[rank, result['sentence']])
+                'summary result count',
+                len(results),
+                len(expected),
+                hint=sample_name)
 
-    def test_summarize_cambodia(self):
-        self.summarize('cambodia')
+            for rank, result in enumerate(results):
+                assert_ex(
+                    'sentence text at rank',
+                    result['text'],
+                    expected[rank]['text'],
+                    hint=[rank, result['text']])
 
-    def test_summarize_cameroon(self):
-        self.summarize('cameroon')
-
-    def test_summarize_canada(self):
-        self.summarize('canada')
+                assert_ex(
+                    'sentence order at rank',
+                    result['order'],
+                    expected[rank]['order'],
+                    hint=[rank, result['text']])
 
     def score_keyword(self, keyword, word_count, expected):
         """Score keyword frequency among other keywords
@@ -116,7 +125,7 @@ class TestSummarizer:
 
         assert_ex(
             'keyword score',
-            result['totalScore'],
+            result['total_score'],
             expected,
             hint=' of '.join([str(keyword['count']), str(word_count)]))
 
@@ -203,166 +212,118 @@ class TestSummarizer:
 
         self._test_get_top_keyword_threshold(keywords, 4)
 
-    def _test_get_top_keywords(self, sample_name):
-        """Test Summarizer.getTopKeywords with data from the selected sample
+    def test_get_top_keywords(self):
+        """Test Summarizer.get_top_keywords with data from the selected sample
 
         Arguments:
             sample_name {str} -- name of data source
         """
-        samp = Sample(DATA_PATH, sample_name)
-        summ = Summarizer()
+        for sample_name in SAMPLES:
+            samp = Sample(DATA_PATH, sample_name)
+            summ = Summarizer()
 
-        source = None
-        category = None
+            source = None
+            category = None
 
-        keywords = samp.d['keywords']
-        expected = self._get_top_keywords(keywords)
+            keywords = samp.d['keywords']
+            expected = self._get_top_keywords(keywords)
 
-        results = summ.get_top_keywords(samp.d['text'], source, category)
+            results = summ.get_top_keywords(samp.d['text'], source, category)
 
-        all_keywords = [kw['word'] for kw in keywords]
-        assert_ex(
-            'result count',
-            len(results),
-            len(expected))
+            all_keywords = [kw['word'] for kw in keywords]
+            assert_ex(
+                'result count',
+                len(results),
+                len(expected))
 
-        for result in results:
-            try:
-                idx = all_keywords.index(result['word'])
+            for result in results:
+                try:
+                    idx = all_keywords.index(result['word'])
+
+                    assert_ex(
+                        'keyword count',
+                        result['count'],
+                        keywords[idx]['count'])
+
+                    test = self._compare_float(
+                        result['total_score'], keywords[idx]['total_score'])
+
+                    assert_ex(
+                        'keyword score',
+                        result['total_score'],
+                        keywords[idx]['total_score'],
+                        test=test)
+
+                except ValueError:
+                    assert False, 'keyword error'
+
+    def test_score_sentence(self):
+        sentences = []
+
+        for sample_name in SAMPLES:
+            samp = Sample(DATA_PATH, sample_name)
+            sentences = samp.d['sentences']
+            summ = Summarizer()
+            title_words = samp.d['title_words']
+            top_keywords = self._get_top_keywords(samp.d['keywords'])
+            keyword_list = summ._pluck_words(top_keywords)
+            num_sents = len(sentences)
+
+            for sentence in sentences:
+                idx = sentence['order']
+                text = sentence['text']
+                expected = sentence['total_score']
+                output = summ.score_sentence(
+                    idx, text,
+                    title_words, top_keywords, keyword_list, num_sents)
+                result = output['total_score']
 
                 assert_ex(
-                    'keyword count',
-                    result['count'],
-                    keywords[idx]['count'])
+                    'sentence score',
+                    result,
+                    expected,
+                    hint=sample_name + ': ' + self._snip(text),
+                    test=self._compare_float(result, expected))
 
-                test = self._compareFloat(
-                    result['totalScore'], keywords[idx]['totalScore'])
-
-                assert_ex(
-                    'keyword score',
-                    result['totalScore'],
-                    keywords[idx]['totalScore'],
-                    test=test)
-
-            except ValueError:
-                assert False, 'keyword error'
-
-    def test_get_top_keywords_cambodia(self):
-        self._test_get_top_keywords('cambodia')
-
-    def test_get_top_keywords_cameroon(self):
-        self._test_get_top_keywords('cameroon')
-
-    def test_get_top_keywords_canada(self):
-        self._test_get_top_keywords('canada')
-
-    def _test_sortScore(self, sample_name):
-        """Test Summarizer.sortScore with data from the selected sample
-
-        Arguments:
-            sample_name {str} -- name of data source
-        """
-        samp = Sample(DATA_PATH, sample_name)
-        summ = Summarizer()
-        sentences = self._randomize_list(samp.d['sentences'])
-
-        results = summ.sortScore(sentences)
-
-        for result in results:
-            try:
-                expected = [
-                    x for x in sentences
-                    if x['order'] == result['order']][0]
-
-                rv = result['totalScore']
-                ev = expected['totalScore']
-                cv = self._compareFloat(rv, ev)
-
-                assert_ex('sort score', rv, ev, test=cv)
-
-            except IndexError:
-                assert False, 'bad sentence list'
-
-    def test_sortScore_cambodia(self):
-        self._test_sortScore('cambodia')
-
-    def test_sortScore_cameroon(self):
-        self._test_sortScore('cameroon')
-
-    def test_sortScore_canada(self):
-        self._test_sortScore('canada')
-
-    def _test_sortSentences(self, sample_name):
-        """Test Summarizer.sortSentences with data from the selected sample
-
-        Arguments:
-            sample_name {str} -- name of data source
-        """
-        samp = Sample(DATA_PATH, sample_name)
-        summ = Summarizer()
-        sentences = self._randomize_list(samp.d['sentences'])
-
-        results = summ.sortSentences(sentences)
-
-        for expected, result in enumerate(results):
-            assert_ex('sort order', result['order'], expected)
-
-    def test_sortSentences_cambodia(self):
-        self._test_sortSentences('cambodia')
-
-    def test_sortSentences_cameroon(self):
-        self._test_sortSentences('cameroon')
-
-    def test_sortSentences_canada(self):
-        self._test_sortSentences('canada')
-
-    def computeScore(self, sample_name):
+    def compute_score(self, sample_name):
         """Test Summarizer.computeScore with data from the selected sample
 
         Arguments:
             sample_name {str} -- name of data source
         """
-        samp = Sample(DATA_PATH, sample_name)
-        summ = Summarizer()
-        sentences = pluck(samp.d['sentences'], 'text')
-        title_words = samp.d['titleWords']
-        top_keywords = self._get_top_keywords(samp.d['keywords'])
+        for sample_name in SAMPLES:
+            samp = Sample(DATA_PATH, sample_name)
+            summ = Summarizer()
+            sentences = pluck(samp.d['sentences'], 'text')
+            title_words = samp.d['title_words']
+            top_keywords = self._get_top_keywords(samp.d['keywords'])
 
-        results = summ.computeScore(sentences, title_words, top_keywords)
+            results = summ.compute_score(sentences, title_words, top_keywords)
 
-        for order, result in enumerate(results):
-            expected = samp.d['sentences'][order]
-            res_snip = result['sentence'][:17] + '...'
-            exp_snip = expected['text'][:17] + '...'
+            for order, result in enumerate(results):
+                expected = samp.d['sentences'][order]
+                res_snip = self._snip(result['text'])
+                exp_snip = self._snip(expected['text'])
 
-            assert_ex(
-                'sentence order',
-                result['order'],
-                order,
-                hint=[res_snip, exp_snip])
+                assert_ex(
+                    'sentence order',
+                    result['order'],
+                    order,
+                    hint=[res_snip, exp_snip])
 
-            assert_ex(
-                'sentence score',
-                result['totalScore'],
-                expected['totalScore'],
-                test=self._compareFloat(
-                    result['totalScore'], expected['totalScore']),
-                hint=[res_snip, exp_snip])
+                assert_ex(
+                    'sentence score',
+                    result['total_score'],
+                    expected['total_score'],
+                    test=self._compare_float(
+                        result['total_score'], expected['total_score']),
+                    hint=[res_snip, exp_snip])
 
-            assert_ex(
-                'sentence text',
-                result['sentence'],
-                expected['text'],
-                hint=[result['sentence'], expected['text']])
-
-    def test_computeScore_cambodia(self):
-        self.computeScore('cambodia')
-
-    def test_computeScore_cameroon(self):
-        self.computeScore('cameroon')
-
-    def test_computeScore_canada(self):
-        self.computeScore('canada')
+                assert_ex(
+                    'sentence text',
+                    result['text'],
+                    expected['text'],
+                    hint=[result['text'], expected['text']])
 
     def _test_sentence_scoring(self, sample_name, score_type):
         """Test Summarizer.sbs or .dbs with data from the selected sample
@@ -375,42 +336,33 @@ class TestSummarizer:
         summ = Summarizer()
 
         for sentence in samp.d['sentences']:
-            unpunct = summ.parser.removePunctations(sentence)
-            words = summ.parser.splitWords(unpunct)
-            topKeywords = self._get_top_keywords(samp.d['keywords'])
-            keywordList = self._get_keyword_list(samp.d['keywords'])
+            words = summ.parser.get_all_words(sentence['text'])
+            top_keywords = self._get_top_keywords(samp.d['keywords'])
+            keyword_list = summ._pluck_words(top_keywords)
 
             expected = sentence[score_type]
 
             if score_type == 'sbs':
-                result = summ.sbs(words, topKeywords, keywordList)
+                result = summ.sbs(words, top_keywords, keyword_list)
 
             if score_type == 'dbs':
-                result = summ.dbs(words, topKeywords, keywordList)
+                result = summ.dbs(words, top_keywords, keyword_list)
 
             assert_ex(
                 score_type,
                 expected,
                 result,
-                test=self._compareFloat(result, expected),
-                hint=unpunct)
+                test=self._compare_float(result, expected),
+                hint=self._snip(words))
 
-    def sbs(self, sample_name):
+    def sbs(self):
         """Test Summarizer.sbs with data from the selected sample
 
         Arguments:
             sample_name {str} -- name of data source
         """
-        self._test_sentence_scoring(sample_name, 'sbs')
-
-    def test_sbs_cambodia(self):
-        self.sbs('cambodia')
-
-    def test_sbs_cameroon(self):
-        self.sbs('cameroon')
-
-    def test_sbs_canada(self):
-        self.sbs('canada')
+        for sample_name in SAMPLES:
+            self._test_sentence_scoring(sample_name, 'sbs')
 
     def dbs(self, sample_name):
         """Test Summarizer.dbs with data from the selected sample
@@ -418,13 +370,5 @@ class TestSummarizer:
         Arguments:
             sample_name {str} -- name of data source
         """
-        self._test_sentence_scoring(sample_name, 'dbs')
-
-    def test_dbs_cambodia(self):
-        self.dbs('cambodia')
-
-    def test_dbs_cameroon(self):
-        self.dbs('cameroon')
-
-    def test_dbs_canada(self):
-        self.dbs('canada')
+        for sample_name in SAMPLES:
+            self._test_sentence_scoring(sample_name, 'dbs')
