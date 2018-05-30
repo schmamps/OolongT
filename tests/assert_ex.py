d20b89cbccd0a74108b35f24c0b7c5ea6db0c498
Andrew Champion <awchampion@gmail.com>
2018-05-30 12:12:08 -0700

    relocate and correct doctests

--- tests/assert_ex.py
+++ tests/assert_ex.py
@@ -10,10 +10,10 @@ def compare_list(left, right):
         right {List[Any]} - list to compare against
 
     Returns:
-        str -- comparison of left to right
+        str -- length of left list, REPR of exclusive values
 
     >>> compare_list([1, 2, 3], [2, 3, 4])
-    'len: 3, exclusive: [1]'
+    "len: 3, exclusive: ['1']"
     >>> compare_list([1, 2, 3], [1, 2, 3, 4])
     'len: 3, exclusive: []'
     """
@@ -23,16 +23,43 @@ def compare_list(left, right):
     return ', '.join([len_str, diff_str])
 
 
-def assert_ex(msg, result, expected, **kwargs):
-    """Generate detailed AssertionError exceptions
+def get_assert_params(kwargs):
+    """Extract kwargs for assert_ex
 
     Arguments:
-        msg {str} -- description of error
-        result {any} -- outcome of test
-        expected {any} -- expected outcome of test
+        kwargs {Dict} - kwargs to assert_ex
 
     Returns:
-        bool -- tested condition
+        Tuple[bool, str] - result of test and assertion detail
+
+    >>> get_assert_params({})
+    (None, '')
+    >>> get_assert_params({'test': (1 <= 2)})
+    (True, '')
+    >>> get_assert_params({'test': (1 == 2)})
+    (False, '')
+    >>> get_assert_params({'hint': 'hi'})
+    (None, " ('hi')")
+    >>> get_assert_params({'test': 1 <= 2, 'hint': 'hi'})
+    (True, " ('hi')")
+    """
+    test = None
+    hint = ''
+
+    for key, val in kwargs.items():
+        if key == 'test':
+            test = val
+            continue
+
+        if key == 'hint':
+            hint = ' (' + repr(val) + ')'
+            continue
+
+    return test, hint
+
+
+def _assert_ex(msg, result, expected, **kwargs):
+    """Shadow assert_ex for doctests
 
     >>> rv = [0]
     >>> ev = [0]
@@ -44,8 +71,8 @@ def assert_ex(msg, result, expected, **kwargs):
     Traceback (most recent call last):
     ...
     AssertionError: Mismatch
-    result   > len: 1, exclusive: [0]
-    expected > len: 1, exclusive: [1]
+    result   > len: 1, exclusive: ['0']
+    expected > len: 1, exclusive: ['1']
     >>> rv = [0]
     >>> rc = len(rv)
     >>> ev = [1]
@@ -73,17 +100,21 @@ def assert_ex(msg, result, expected, **kwargs):
     result   > 1
     expected > 1.0
     """
-    test = None
-    hint = ''
+    assert_ex(msg, result, expected, **kwargs)
 
-    for key, val in kwargs.items():
-        if key == 'test':
-            test = val
-            continue
 
-        if key == 'hint':
-            hint = ' (' + repr(val) + ')'
-            continue
+def assert_ex(msg, result, expected, **kwargs):
+    """Generate detailed AssertionError exceptions
+
+    Arguments:
+        msg {str} -- description of error
+        result {any} -- outcome of test
+        expected {any} -- expected outcome of test
+
+    Returns:
+        bool -- tested condition
+    """
+    test, hint = get_assert_params(kwargs)
 
     if test is None:
         test = (result == expected)
