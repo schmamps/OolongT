c58bea1abb185e606b98282858d774be9cb6d4a2
Andrew Champion <awchampion@gmail.com>
2018-05-31 14:37:07 -0700

    function docstrings multiple key sort in sort_by

--- oolongt/nodash.py
+++ oolongt/nodash.py
@@ -1,6 +1,46 @@
 def pluck(iterable, key):
+    """Get value at specified key in a list of Dicts
+
+    Arguments:
+        iterable {iterable} -- an iterable of Dicts
+        key {any} -- key in each dict to extract
+
+    Returns:
+        list[any] -- all values at specified key
+    """
     return [item[key] for item in iterable]
 
 
-def sort_by(iterable, keyName, reverse=False):
-    return sorted(iterable, key=lambda x: x[keyName], reverse=reverse)
+def get_sort_tuple(item, key):
+    """Create tuple of values suitable for sorted()
+
+    Arguments:
+        item {Dict} -- Dict to be sorted
+        key {any, List[any]} -- sorting criteria, descending order
+
+    Returns:
+        any -- value(s) for sorting
+    """
+    if isinstance(key, list):
+        values = [item[x] for x in key]
+
+        return tuple(values)
+
+    return item[key]
+
+
+def sort_by(iterable, key, reverse=False):
+    """Sort iterable of Dicts using value of one or more keys
+
+    Arguments:
+        iterable {iterable} -- iterable list of Dicts
+        key {any or List[any]} -- key(s) to use for sort
+
+    Keyword Arguments:
+        reverse {bool} -- False: ASC, True: DESC (default: {False})
+
+    Returns:
+        List[any] -- sorted iterable
+    """
+    return sorted(
+        iterable, key=lambda x: get_sort_tuple(x, key), reverse=reverse)
