12a2a0d38bffe955a2793f3d04ee0081196781f2
Andrew Champion <awchampion@gmail.com>
2018-05-31 14:28:38 -0700

    change project exports

--- oolongt/__init__.py
+++ oolongt/__init__.py
@@ -1,16 +1,1 @@
-# !/usr/bin/python
-# -*- coding: utf-8 -*-
-from .summarizer import Summarizer
-
-
-class TextTeaser(object):
-    def __init__(self):
-        self.summarizer = Summarizer()
-
-    def summarize(self, title, text,
-                  category="Undefined", source="Undefined", count=5):
-        result = self.summarizer.summarize(text, title, source, category)
-        result = self.summarizer.sort_sentences(result[:count])
-        result = [res['text'] for res in result]
-
-        return result
+from .main import summarize, rank_sentences
