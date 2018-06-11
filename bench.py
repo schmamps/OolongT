a9ae7f0065cc886eca5bf16412210519daee86c2
Andrew Champion <awchampion@gmail.com>
2018-06-10 19:01:14 -0700

    simple benchmarker

---
+++ bench.py
@@ -0,0 +1,34 @@
+from io import open as io_open
+from math import floor
+from time import time
+
+from oolongt.nodash import pluck, sort_by
+from oolongt.summarizer import Summarizer
+
+from tests.constants import DATA_PATH, SAMPLES
+from tests.helpers import (
+    assert_ex, compare_float, compare_dict, get_samples, randomize_list, snip)
+from tests.sample import Sample
+
+
+def report(title, *data):
+    results = [{'desc': x[0], 'time': x[1]} for x in data]
+    pad = max([len(x['desc']) for x in results])
+    formatter = '{{0:{0}s}}: {{1:.6f}}s'.format(pad)
+    by_speed = sorted(results, key=lambda x: x['time'])
+
+    lines = [title.upper(), '=']
+    for result in by_speed:
+        lines += [formatter.format(result['desc'], result['time'])]
+
+    for num, line in enumerate(lines):
+        text = line
+        if num == 1:
+            text *= max([len(x) for x in lines])
+
+        print(text)
+
+    print(
+        '\ndifference: {0:.3f}x'.format(
+            by_speed[-1]['time'] / by_speed[0]['time']))
+    print('')
