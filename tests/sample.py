473ccc67a451aa1c50aa867ae77e76fb5bc4aa29
Andrew Champion <awchampion@gmail.com>
2018-06-23 13:50:06 -0700

    add discrete .text and .title properties update and/or correct docstrings PEP 484 hints

--- tests/sample.py
+++ tests/sample.py
@@ -1,10 +1,11 @@
 """ Load sample data """
-from oolongt import simple_io
+from oolongt import simple_io, nodash
 from pathlib import Path
 
 
 class Sample:
     def __init__(self, root, name):
+        # type: (str, str) -> None
         base = str(root.joinpath(name))
         data = simple_io.load_json(base + '.json')
 
@@ -15,20 +16,23 @@ class Sample:
         self.name = name
 
     def join_sentences(self, sentence_list):
+        # type: (list[dict]) -> str
         """Create text from sentence list
 
         Arguments:
-            sentence_list {List[Dict]} -- loaded sentences
+            sentence_list {list[dict]} -- loaded sentences
 
         Returns:
             str -- text
         """
-        return '\n  '.join([sent['text'] for sent in sentence_list])
+        return '\n  '.join(nodash.pluck(sentence_list, 'text'))
 
     @property
     def text(self):
+        # type: () -> str
         return self.d['text']
 
     @property
     def title(self):
+        # type: () -> str
         return self.d['title']
