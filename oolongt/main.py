8bc26e0f246a46266814b51effbae0ed461600f6
Andrew Champion <awchampion@gmail.com>
2018-07-05 16:45:56 -0700

    remove sorting from summarize() refactor summarize() code into get_best_sentences()

--- oolongt/main.py
+++ oolongt/main.py
@@ -3,12 +3,12 @@ from . import parser
 from .nodash import pluck, sort_by
 from .summarizer import Summarizer
 
-DEFAULT_SORT_KEY = 'order'
+DEFAULT_SORT_KEY = 'index'
 DEFAULT_REVERSE = False
 DEFAULT_LENGTH = 5
 
 
-def score_sentences(title, text,
+def score_sentences(title, body,
                     root=parser.BUILTIN, lang=parser.DEFAULT_LANG,
                     source=None, category=None):
     # type: (str, str, str, str, any, any) -> list[dict]
@@ -16,7 +16,7 @@ def score_sentences(title, text,
 
     Arguments:
         title {str} -- title of content
-        text {str} -- body of content
+        body {str} -- body of content
 
     Keyword Arguments:
         root {str} -- root directory of language config
@@ -28,48 +28,9 @@ def score_sentences(title, text,
         list[dict] -- List of sentences with scoring and metadata
     """
     summarizer = Summarizer()
-    sentences = summarizer.get_sentences(text, title, source, category)
+    sentences = summarizer.get_sentences(body, title, source, category)
 
     return sentences
-
-
-def summarize(title, text,
-              length=DEFAULT_LENGTH,
-              sort_key=DEFAULT_SORT_KEY, reverse=DEFAULT_REVERSE,
-              root=parser.BUILTIN, lang=parser.DEFAULT_LANG,
-              source=None, category=None):
-    # type: (str, str, int, str, bool, str, str, any, any) -> list[dict]
-    """Get `length` sentences from `text` sorted by `sort_key`
-
-    Where length >= 1, length is an absolute number of sentences
-    Where length  < 1, length is a fraction of the total sentence count
-
-    Arguments:
-        title {str} -- title of content
-        text {str} -- body of content
-
-    Keyword Arguments:
-        length {int or float < 1} -- lines to return (int) or
-            fraction of total (float) (default: {5})
-        sort_key {str} -- sort sentences by specified key
-            (default: {'order'})
-        reverse {bool} -- descending order (default: {False})
-        source {any} -- unused (default: {None})
-        category {any} -- unused (default: {None})
-
-    Returns:
-        list[str] -- top sentences sorted by criteria
-    """
-    sentences = sort_by(
-        score_sentences(title, text, root, lang, source, category),
-        ['total_score', 'order'],
-        reverse=True)
-    slice_length = get_slice_length(length, len(sentences))
-
-    ordered = sort_by(
-        sentences[:slice_length], sort_key, reverse=reverse)
-
-    return pluck(ordered, 'text')
 
 
 def get_slice_length(nominal, total):
@@ -98,3 +59,51 @@ def get_slice_length(nominal, total):
         return max([1, int(nominal * total)])
 
     return min([int(nominal), total])
+
+
+def get_best_sentences(title, body,
+                       length=DEFAULT_LENGTH,
+                       root=parser.BUILTIN, lang=parser.DEFAULT_LANG,
+                       source=None, category=None):
+    # type: (str, str, int, str, str, any, any) -> list[ScoredSentence]
+    """Get `length` best sentences from `body` in score order
+
+    Returns:
+        list[ScoredSentence] -- top sentences from `body`
+    """
+    sentences = score_sentences(
+        title, body, root, lang, source, category)
+    slice_length = get_slice_length(length, len(sentences))
+
+    return sorted(sentences, reverse=True)[:slice_length]
+
+
+def summarize(title, body,
+              length=DEFAULT_LENGTH,
+              root=parser.BUILTIN, lang=parser.DEFAULT_LANG,
+              source=None, category=None):
+    # type: (str, str, int, str, str, any, any) -> list[str]
+    """Get `length` best sentences from `body` in content order
+
+    if `length` < 1:
+        len(return) = int(length * len(sentences))
+    else:
+        len(return) = min(length, len(sentences))
+
+    Arguments:
+        title {str} -- title of content
+        body {str} -- body of content
+
+    Keyword Arguments:
+        length {int or float < 1} -- sentences to return (int) or
+            fraction of total (float) (default: {5})
+        source {any} -- unused (default: {None})
+        category {any} -- unused (default: {None})
+
+    Returns:
+        list[str] -- top sentences sorted by criteria
+    """
+    sentences = get_best_sentences(
+        title, body, length, root, lang, source, category)
+
+    return [s.text for s in sorted(sentences, key=lambda x: x.index)]
