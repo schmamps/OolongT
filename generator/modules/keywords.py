f24008434a1ec913265136373a37eb04636281e5
Andrew Champion <awchampion@gmail.com>
2018-07-05 16:42:25 -0700

    update for ScoredKeyword type

--- generator/modules/keywords.py
+++ generator/modules/keywords.py
@@ -6,21 +6,26 @@ from generator.util import get_samples, json as json_util
 
 
 def jsonify(keyword):
-    formatter = '\t\t{{"word": "{0}", "count": {1:d}, "total_score": {2:.8f}}}'
-    return formatter.format(
-        keyword['word'], keyword['count'], keyword['total_score'])
+    data = {
+        'score': keyword.score,
+        'count': keyword.count,
+        'word': keyword.word, }
+
+    pairs = [
+        json_util.kv_pair(data, 'score', '.12f'),
+        json_util.kv_pair(data, 'count', '2d'),
+        json_util.kv_pair(data, 'word'), ]
+
+    return '\t\t{' + ', '.join(pairs) + '}'
 
 
 def generate():
-    summ = Summarizer()
     p = Parser()
 
     for samp in get_samples():
-        receiveds, word_count = p.get_keywords(samp.text)
-        keywords = sorted(
-            receiveds,
-            key=lambda x: (-x['count'], x['word']))
-        keywords = [summ.score_keyword(w, word_count) for w in keywords]
+        receiveds = p.get_keywords(samp.body)
+        keywords = sorted(receiveds, reverse=True)
+        word_count = sum([kw.count for kw in keywords])
 
         with json_util.create(samp.name, 'keywords') as file:
             file.write('{\n')
@@ -29,19 +34,14 @@ def generate():
             file.write(',\n'.join([jsonify(kw) for kw in keywords]))
             file.write(json_util.close(']'))
 
-        try:
-            basic_words = pluck(samp.keywords or {}, 'word')
+        # basic_words = [kw.word for kw in samp.keywords]
+        # stem_words = [kw.word for kw in keywords]
 
-        except AttributeError:
-            basic_words = []
+        # if len(basic_words):
+        #     for word in sorted(list(set(basic_words + stem_words))):
+        #         if word not in basic_words and len(basic_words) > 0:
+        #             print('unique stem: ' + word)
 
-        stem_words = pluck(keywords, 'word')
-
-        if len(basic_words):
-            for word in sorted(list(set(basic_words + stem_words))):
-                if word not in basic_words and len(basic_words) > 0:
-                    print('unique stem: ' + word)
-
-                if word not in stem_words:
-                    print('unique basic: ' + word)
+        #         if word not in stem_words:
+        #             print('unique basic: ' + word)
         print('')
