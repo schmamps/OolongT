4b9fd42c8ddeed65760017de8e2a3f8cf30a8c25
Andrew Champion <awchampion@gmail.com>
2018-07-30 15:45:25 -0700

    refactor module constants

---
+++ oolongt/constants.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+
+# summarizer
+DEFAULT_LENGTH = 5
+
+# approximation
+DEFAULT_TOLERANCE = 0.0000001  # simple floats
+COMPOSITE_TOLERANCE = 0.00000011   # composite scores
+
+# parser
+BUILTIN = Path(__file__).parent.joinpath('lang')
+DEFAULT_LANG = 'en'
+DEFAULT_IDEAL_LENGTH = 20
+DEFAULT_NLTK_LANGUAGE = 'english'
+DEFAULT_INITIAL_STOPS = 'nltk'
+DEFAULT_CUSTOM_STOPS = []
+
+# scored keyword
+KEYWORD_SCORE_K = 1.5
