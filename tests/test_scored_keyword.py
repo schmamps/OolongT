06580f673e6f97b58c54c2d4de0d9ed315c1b21e
Andrew Champion <awchampion@gmail.com>
2018-08-01 17:14:31 -0700

    update pytest.mark import add test IDs improve parametrize order fix __cmp__-class assertions

--- tests/test_scored_keyword.py
+++ tests/test_scored_keyword.py
@@ -1,10 +1,10 @@
-import pytest
+from pytest import mark
 
 from oolongt.typing.scored_keyword import (
     score_keyword,
     compare_length, compare_score, compare_word, compare_keywords,
     KEYWORD_SCORE_K, ScoredKeyword)
-from tests.helpers import assert_ex, check_exception
+from tests.helpers import assert_ex, check_exception, pad_to_longest
 
 
 def set_test(a_params, b_params, *args):
@@ -25,18 +25,38 @@ def get_value_tests():
     return [
         set_test(('foo', 1, 1), ('foo', 1, 1), False, True),
         set_test(('foo', 1, 1), ('foo', 2, 2), False, True),
-        set_test(('foo', 1, 1), ('foobar', 1, 1), True, False),
-        set_test(('foo', 1, 1), ('bar', 1, 1), False, False),
         set_test(('foo', 1, 2), ('foo', 1, 1), True, False),
-        set_test(('foo', 1, 1), ('foo', 1, 2), False, False), ]
+        set_test(('foo', 1, 1), ('foo', 1, 2), False, False),
+        set_test(('foo', 1, 1), ('foobar', 1, 1), True, False),
+        set_test(('foobar', 1, 1), ('foo', 1, 1), False, False),
+    ]
 
 
-@pytest.mark.parametrize('count,of,expected', [
-    (1, 1, 1.5),
-    (1, 2, .75),
-    (1, 10, .15),
-    (1, 0, ValueError),
-])
+def get_value_ids():
+    return [
+        '(word: a=b, count: a=b, of: a=b, score: a=b) == eq',
+        '(word: a=b, count: a<b, of: a<b, score: a=b) == eq',
+        '(word: a=b, count: a=b, of: a>b, score: a=b) == lt',
+        '(word: a=b, count: a=b, of: a<b, score: a=b) == gt',
+        '(word: a<b, count: a=b, of: a=b, score: a=b) == lt',
+        '(word: a>b, count: a=b, of: a=b, score: a=b) == gt',
+    ]
+
+
+@mark.parametrize(
+    'count,of,expected',
+    [
+        (1, 1, 1.5),
+        (1, 2, .75),
+        (1, 10, .15),
+        (1, 0, ValueError),
+    ],
+    ids=[
+        '1 of    1',
+        '1 of    2',
+        '1 of   10',
+        '1 of 0(!)',
+    ])
 def test_score_keyword(count, of, expected):
     # type: (int, int, any) -> None
     try:
@@ -52,12 +72,24 @@ def test_score_keyword(count, of, expected):
         hint=' of '.join([count, of]))
 
 
-@pytest.mark.parametrize('a,b,expected', [
-    set_test(('foo', 1, 1), ('foo', 1, 1), 0),
-    set_test(('foo', 1, 1), ('bar', 1, 1), 0),
-    set_test(('foo', 1, 1), ('foo', 1, 2), 1),
-    set_test(('foo', 1, 2), ('foo', 1, 1), -1),
-])
+@mark.parametrize(
+    'a,b,expected',
+    [
+        set_test(('foo', 1, 1), ('foo', 1, 1), 0),
+        set_test(('foo', 1, 1), ('bar', 1, 1), 0),
+        set_test(('foo', 1, 2), ('foo', 1, 1), -1),
+        set_test(('foo', 1, 2), ('bar', 1, 1), -1),
+        set_test(('foo', 1, 1), ('foo', 1, 2), 1),
+        set_test(('foo', 1, 1), ('bar', 1, 2), 1),
+    ],
+    ids=[
+        '(word: eq, score: a=b) == 0',
+        '(word: ne, score: a=b) == 0',
+        '(word: eq, score: a<b) == -',
+        '(word: ne, score: a<b) == -',
+        '(word: eq, score: a>b) == +',
+        '(word: ne, score: a>b) == +',
+    ])
 def test_compare_score(a, b, expected):
     received = compare_score(a, b)
 
@@ -68,12 +100,19 @@ def test_compare_score(a, b, expected):
         hint=[a, b])
 
 
-@pytest.mark.parametrize('a,b,expected', [
-    set_test(('foo', 1, 1), ('foo', 1, 1), 0),
-    set_test(('foo', 1, 1), ('bar', 1, 1), 0),
-    set_test(('foo', 1, 1), ('fo', 1, 1), 1),
-    set_test(('foo', 1, 1), ('quux', 1, 1), -1),
-])
+@mark.parametrize(
+    'a,b,expected',
+    [
+        set_test(('foo', 1, 1), ('foo', 1, 1), 0),
+        set_test(('foo', 1, 1), ('bar', 1, 1), 0),
+        set_test(('foo', 1, 1), ('quux', 1, 1), -1),
+        set_test(('quux', 1, 1), ('foo', 1, 1), 1),
+    ], ids=[
+        '(word: eq, len: a=b) == 0',
+        '(word: ne, len: a=b) == 0',
+        '(word: ne, len: a<b) == -',
+        '(word: ne, len: a>b) == +',
+    ])
 def test_compare_length(a, b, expected):
     received = compare_length(a, b)
 
@@ -84,11 +123,18 @@ def test_compare_length(a, b, expected):
         hint=[a, b])
 
 
-@pytest.mark.parametrize('a,b,expected', [
-    set_test(('foo', 1, 1), ('foo', 1, 1), 0),
-    set_test(('foo', 1, 1), ('bar', 1, 1), 1),
-    set_test(('foo', 1, 1), ('qux', 1, 1), -1),
-])
+@mark.parametrize(
+    'a,b,expected',
+    [
+        set_test(('foo', 1, 1), ('foo', 1, 1), 0),
+        set_test(('foo', 1, 1), ('bar', 1, 1), 1),
+        set_test(('foo', 1, 1), ('qux', 1, 1), -1),
+    ],
+    ids=[
+        '(a eq b) == 0',
+        '(a gt b) == +',
+        '(a lt b) == -',
+    ])
 def test_compare_word(a, b, expected):
     received = compare_word(a, b)
 
@@ -99,16 +145,19 @@ def test_compare_word(a, b, expected):
         hint=[a, b])
 
 
-@pytest.mark.parametrize('a,b,expected', [
-    set_test(('foo', 1, 1), ('foo', 1, 1), (0, 0, 0)),
-    set_test(('foo', 1, 1), ('foo', 1, 2), (1, 0, 0)),
-    set_test(('foo', 1, 1), ('quux', 1, 1), (0, -1, -1)),
-    set_test(('foo', 1, 1), ('fo', 1, 1), (0, 1, 1)),
-    set_test(('foo', 1, 1), ('bar', 1, 1), (0, 0, 1)),
-    set_test(('foo', 1, 1), ('qux', 1, 1), (0, 0, -1)),
-    set_test(('foo', 1, 1), ('ba', 1, 2), (1, 1, 1)),
-    set_test(('foo', 1, 2), ('quux', 1, 1), (-1, -1, -1)),
-])
+# trip all factors (individual factors tested separately)
+@mark.parametrize(
+    'a,b,expected',
+    [
+        set_test(('foo', 1, 2), ('quux', 1, 1), (-1, -1, -1)),
+        set_test(('foo', 1, 1), ('foo', 1, 1), (0, 0, 0)),
+        set_test(('foo', 1, 1), ('ba', 1, 2), (1, 1, 1)),
+    ],
+    ids=[
+        '(score: a<b, len: a<b, str: a<b)[:] == (-) * 3',
+        '(score: a=b, len: a=b, str: a=b)[:] == (0) * 3',
+        '(score: a>b, len: a>b, str: a>b)[:] == (+) * 3',
+    ])
 def test_compare_keywords(a, b, expected):
     received = compare_keywords(a, b)
 
@@ -120,21 +169,31 @@ def test_compare_keywords(a, b, expected):
 
 
 class TestScoredKeyword:
-    @pytest.mark.parametrize('keyword,_,expected', [
-        set_test(('foo', 1, 1), ('', 1, 1), 'foo'),
-        set_test(('bar', 1, 1), ('', 1, 1), 'bar'),
-        set_test(('Validation? No!', 1, 1), ('', 1, 1), 'validation? no!'),
-    ])
+    @mark.parametrize(
+        'keyword,_,expected',
+        [
+            set_test(('foo', 1, 1), ('', 1, 1), 'foo'),
+            set_test(('bar', 1, 1), ('', 1, 1), 'bar'),
+            set_test(('Validation? No!', 1, 1), ('', 1, 1), 'validation? no!'),
+        ],
+        ids=pad_to_longest([
+            'foo',
+            'bar',
+            'unvalidated',
+        ]))
     def test_str(self, keyword, _, expected):
         received = str(keyword)
 
         assert (received == expected), assert_ex(
-            'keyword to string',
+            'keyword string cast',
             received,
             expected,
             hint=keyword)
 
-    @pytest.mark.parametrize('a,b,_,expected', get_value_tests())
+    @mark.parametrize(
+        'a,b,_,expected',
+        get_value_tests(),
+        ids=get_value_ids())
     def test_eq(self, a, b, _, expected):
         received = (a == b)
 
@@ -142,28 +201,31 @@ class TestScoredKeyword:
             'keyword equality',
             received,
             expected,
-            hint=[a, b]
-        )
+            hint=[a, b])
 
-    @pytest.mark.parametrize('a,b,expected,_', get_value_tests())
+    @mark.parametrize(
+        'a,b,expected,_',
+        get_value_tests(),
+        ids=get_value_ids())
     def test_lt(self, a, b, expected, _):
         received = (a < b)
 
         assert (received == expected), assert_ex(
-            'keyword equality',
+            'keyword lt',
             received,
             expected,
-            hint=[a, b]
-        )
+            hint=[a, b])
 
-    @pytest.mark.parametrize('a,b,lt,eq', get_value_tests())
+    @mark.parametrize(
+        'a,b,lt,eq',
+        get_value_tests(),
+        ids=get_value_ids())
     def test_gt(self, a, b, lt, eq):
         expected = not (lt or eq)
         received = (a > b)
 
         assert (received == expected), assert_ex(
-            'keyword equality',
+            'keyword gt',
             received,
             expected,
-            hint=[a, b]
-        )
+            hint=[a, b])
