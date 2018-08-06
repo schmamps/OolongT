6a03ee42cb30a6c17dacada2ea38ff9b2da621be
Andrew Champion <awchampion@gmail.com>
2018-08-06 14:53:05 -0700

    fix function name get_median_keywords()

--- generator/modules/keywords.py
+++ generator/modules/keywords.py
@@ -18,7 +18,7 @@ def dictify(keyword):
     return kw_dict
 
 
-def get_mean_keywords(samp):
+def get_median_keywords(samp):
     p = Parser()
     samples = [p.get_keywords(samp.body) for _ in range(SAMPLING_SIZE)]
 
@@ -39,7 +39,7 @@ def generate():
         file_path = json_util.get_output_path(file_comps)
 
         try:
-            received = get_mean_keywords(samp)
+            received = get_median_keywords(samp)
             keywords = sorted(received, reverse=True)
             keyword_count = sum([kw.count for kw in keywords])
 
