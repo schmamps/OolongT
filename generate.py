fbc0623d9bdbf5812f1fb5aa2f59c8add3630713
Andrew Champion <awchampion@gmail.com>
2018-08-06 14:18:48 -0700

    add merge use console functions

--- generate.py
+++ generate.py
@@ -1,4 +1,10 @@
-from generator import keywords, sentences
+from generator.modules import keywords, sentences, merge
+from generator.util import console
+
+console.group('Sample Data Generator')
 
 keywords.generate()
 sentences.generate()
+merge.generate()
+
+console.group_end()
