5e2a0e4631dee58da234bc50392e8f547bf095b3
Andrew Champion <awchampion@gmail.com>
2018-08-06 14:16:15 -0700

    refactor out get_sample_paths() simplify for console

--- generator/util/__init__.py
+++ generator/util/__init__.py
@@ -4,14 +4,16 @@ from tests import helpers  # circular dependency hack
 from tests.typing.sample import Sample
 
 
+def get_sample_paths():
+    root = Path(__file__).parent.parent.parent.joinpath('tests', 'data')
+
+    # pylint: disable=no-member
+    return root.glob('*.json')
+
+
 def get_samples():
     root = Path(__file__).parent.parent.parent.joinpath('tests', 'data')
 
     # pylint: disable=no-member
-    for file in root.glob('*.json'):
-        stem = file.stem
-
-        print(stem.upper())
-        print('=' * len(stem))
-
+    for file in get_sample_paths():
         yield Sample(root, file.stem)
