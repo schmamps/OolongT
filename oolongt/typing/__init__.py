d69466e106dacaf135355150f98eabb0e9a32652
Andrew Champion <awchampion@gmail.com>
2018-08-07 14:18:58 -0700

    fix missing references

--- oolongt/typing/__init__.py
+++ oolongt/typing/__init__.py
@@ -3,9 +3,11 @@ import sys
 
 if sys.version_info[0] < 3:
     sys.path.append('..')
-    JSONDecodeError = ValueError
     PermissionError = IOError
+    FileNotFoundError = IOError
+    JSONDecodeError = ValueError
 
 else:
-    from json import JSONDecodeError
     PermissionError = PermissionError
+    FileNotFoundError = FileNotFoundError  # pylint: disable=undefined-variable
+    from json import JSONDecodeError       # pylint: disable=no-name-in-module
