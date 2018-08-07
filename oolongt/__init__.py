5683c7ad58d4f84bd7c51691857954838a818e9b
Andrew Champion <awchampion@gmail.com>
2018-08-07 14:06:06 -0700

    simplify Python 2.7 detection

--- oolongt/__init__.py
+++ oolongt/__init__.py
@@ -1,8 +1,11 @@
 import sys
 
 
-if sys.version_info[0] < 3:
+PYTHON_2 = (sys.version_info[0] < 3)
+
+
+if PYTHON_2:
     sys.path.append('..')
 
 
-from oolongt.main import summarize, score_body_sentences  # nopep8
+from oolongt.main import summarize, score_body_sentences  # noqa
