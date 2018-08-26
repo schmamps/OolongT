b6b74d78ec0464dcfaeb8ef9abdd3cb881e08db4
Andrew Champion <awchampion@gmail.com>
2018-08-26 11:42:20 -0700

    add constants for text, docs, and idioms

--- tests/constants.py
+++ tests/constants.py
@@ -1,4 +1,7 @@
 from pathlib import Path
 
 DATA_PATH = Path(__file__).parent.joinpath('data')
+TEXT_PATH = DATA_PATH.joinpath('text')
+DOC_PATH = DATA_PATH.joinpath('docs')
+IDIOM_PATH = DATA_PATH.joinpath('idioms')
 SAMPLES = ['cambodia', 'cameroon', 'canada']
