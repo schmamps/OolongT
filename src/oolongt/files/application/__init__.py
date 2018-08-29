35159b3637285b08b8530523a15b42e6b00215bf
Andrew Champion <awchampion@gmail.com>
2018-08-28 20:20:33 -0700

    remove MIME-type document handlers

--- src/oolongt/files/application/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Initialize application/* MIME types"""
-from oolongt.files.application.docx import parse as docx      # noqa
-from oolongt.files.application.msword import parse as msword  # noqa
-from oolongt.files.application.pdf import parse as pdf        # noqa
-from oolongt.files.application.rtf import parse as rtf        # noqa
--- src/oolongt/files/text/__init__.py
+++
@@ -1,3 +0,0 @@
-"""Initialize text/* MIME types"""
-from oolongt.files.text.html import parse as html    # noqa
-from oolongt.files.text.plain import parse as plain  # noqa
