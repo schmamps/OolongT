c8e0f0682b3060dcbf3d1831cc3d6f97fb0efd3c
Andrew Champion <awchampion@gmail.com>
2018-08-28 20:24:29 -0700

    use urllib for local/remote I/O

--- src/oolongt/simple_io.py
+++ src/oolongt/simple_io.py
@@ -1,7 +1,9 @@
 """Quick and dirty file readers"""
 import typing
-from io import open as io_open
 from json import loads, JSONDecodeError
+from pathlib import Path
+from re import findall
+from urllib import request
 
 
 def load_json(path: typing.Any) -> typing.Dict:
@@ -22,7 +24,42 @@ def load_json(path: typing.Any) -> typing.Dict:
     return loads(contents)
 
 
-def _get_contents(path: typing.Any) -> str:
+def is_url(path: str):
+    return len(findall(r'^(file|ftp|https*):', path)) > 0
+
+
+def get_local_url(path: str) -> str:
+    abs_path = str(Path(path).absolute())
+    url = 'file:' + request.pathname2url(abs_path)
+
+    return url
+
+
+def get_file_url(path: str) -> str:
+    url = path if is_url(path) else get_local_url(path)
+
+    return url
+
+
+def get_stream(path: typing.Any) -> typing.IO[typing.Any]:
+    """Stream read file at `path`
+
+    Arguments:
+        path {typing.Any} -- str or pathlib.Path
+
+    Keyword Arguments:
+        binary {bool} -- read as binary (default: {False})
+
+    Returns:
+        typing.IO[typing.Any] -- io.TextIO or io.BufferedIOBase
+    """
+    url = get_file_url(str(path))
+    stream = request.urlopen(url)
+
+    return stream
+
+
+def get_contents(path: typing.Any, binary=False) -> typing.Any:
     """Read file at `path` into string
 
     Arguments:
@@ -31,11 +68,11 @@ def _get_contents(path: typing.Any) -> str:
     Returns:
         str -- contents of file
     """
-    with io_open(str(path), 'r', encoding='utf-8') as fp:
-        contents = fp.read()
-        fp.close()
 
-    return contents
+    with get_stream(path) as stream:
+        contents = stream.read()  # type: bytes
+
+    return contents if binary else contents.decode('utf-8')
 
 
 def read_file(path: typing.Any) -> str:
@@ -48,7 +85,7 @@ def read_file(path: typing.Any) -> str:
             str -- text in file
     """
     try:
-        contents = _get_contents(path)
+        contents = get_contents(path)
 
     except (IOError, NotADirectoryError) as e:
         raise IOError(str(e))
