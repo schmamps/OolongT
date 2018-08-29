b435d9731f07b10f8dc1c14302d441b0234bceb8
Andrew Champion <awchampion@gmail.com>
2018-08-28 20:31:01 -0700

    export document classes

--- src/oolongt/typedefs/__init__.py
+++ src/oolongt/typedefs/__init__.py
@@ -1,4 +1,10 @@
-from .content import Content                 # noqa
-from .parser_config import ParserConfig      # noqa
-from .scored_keyword import ScoredKeyword    # noqa
+import typing  # noqa
+
+from .content import Content  # noqa
+from .docx_document import DocxDocument  # noqa
+from .html_document import HtmlDocument  # noqa
+from .parser_config import ParserConfig  # noqa
+from .pdf_document import PdfDocument  # noqa
+from .plain_text_document import PlainTextDocument  # noqa
+from .scored_keyword import ScoredKeyword  # noqa
 from .scored_sentence import ScoredSentence  # noqa
