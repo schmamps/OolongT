519798b54741df4a6bf64a943af88666638fb55f
Andrew Champion <awchampion@gmail.com>
2018-08-28 20:30:38 -0700

    process documents in classes add title to summarize function returns

--- src/oolongt/files/files.py
+++ src/oolongt/files/files.py
@@ -5,11 +5,42 @@ from pathlib import Path
 
 import oolongt.text as main
 from oolongt.constants import BUILTIN, DEFAULT_IDIOM, DEFAULT_LENGTH
-from oolongt.files import application, text
-from oolongt.typedefs import Content, ScoredSentence
+from oolongt.typedefs import (Content, DocxDocument, HtmlDocument, PdfDocument,
+                              PlainTextDocument, ScoredSentence)
+from oolongt.typedefs.document import Document  # noqa
 
 
-def load_file(path: str) -> Content:
+def get_handlers() -> typing.Generator[typing.Type[Document], None, None]:
+    """List available document handlers
+
+    Returns:
+        Generator[Document, None, None] -- document handler
+    """
+    yield DocxDocument
+    yield PdfDocument
+    yield HtmlDocument
+
+
+def get_handler(path: str, ext: str):
+    """Determine which class to handle document
+
+    Arguments:
+        path {str} -- path/URL to document
+        ext {str} -- override default extension
+
+    Returns:
+        typdefs.Document -- object with body, title, and keywords properties
+    """
+    ext = (ext or Path(path).suffix).replace('.', '')
+
+    for handler in get_handlers():
+        if handler.supports(path, ext):
+            return handler
+
+    return PlainTextDocument
+
+
+def load_file(path: str, ext=False) -> Content:
     """Get text contents of the file at `path`
 
     Arguments:
@@ -21,26 +52,25 @@ def load_file(path: str) -> Content:
     Returns:
         Content -- contents of file
     """
-    ext = Path(path).suffix
+    handler = get_handler(path, ext)  # type: typing.Callable
 
     try:
-        func = text.plain
-
-        if ext == '.doc':
-            func = application.msword
-        elif ext == '.docx':
-            func = application.docx
-        elif ext == '.pdf':
-            func = application.pdf
-        elif ext == '.rtf':
-            func = application.rtf
-        elif ext == '.htm' or '.html':
-            func = text.html
-
-        return func(path)
+        return handler(path)
 
     except (OSError) as e:  # pylint: disable=invalid-name
         raise ValueError('Unable to read {!r} ({})'.format(abspath(path), e))
+
+
+def entitle(kwargs: typing.Dict[str, typing.Any]):
+    """Get title from summarizer function args
+
+    Arguments:
+        kwargs {typing.Dict[str, typing.Any]} -- summarizing kwargs
+
+    Returns:
+        str -- document title
+    """
+    return kwargs.get('title', 'Untitled')
 
 
 def prep_args(  # pylint: disable=too-many-arguments
@@ -88,8 +118,8 @@ def score_body_sentences(
         path: str,
         root: typing.Any = BUILTIN,
         idiom: typing.Any = DEFAULT_IDIOM,
-        source: typing.Any = None,
-        category: typing.Any = None) -> typing.List[ScoredSentence]:
+        source=None,
+        category=None) -> typing.Tuple[typing.List[ScoredSentence], str]:
     """List and score every sentence in `body`
 
     Arguments:
@@ -108,7 +138,7 @@ def score_body_sentences(
     """
     kwargs = prep_args(path, root, idiom, source, category)
 
-    return main.score_body_sentences(**kwargs)
+    return main.score_body_sentences(**kwargs), entitle(kwargs)
 
 
 def summarize(  # pylint: disable=too-many-arguments
@@ -117,7 +147,7 @@ def summarize(  # pylint: disable=too-many-arguments
         root: typing.Any = BUILTIN,
         idiom: typing.Any = DEFAULT_IDIOM,
         source: typing.Any = None,
-        category: typing.Any = None) -> typing.List[str]:
+        category: typing.Any = None) -> typing.Tuple[typing.List[str], str]:
     """Get `length` best sentences from `body` in content order
 
     if `length` < 1:
@@ -144,4 +174,4 @@ def summarize(  # pylint: disable=too-many-arguments
     """
     kwargs = prep_args(path, root, idiom, source, category, length)
 
-    return main.summarize(**kwargs)
+    return main.summarize(**kwargs), entitle(kwargs)
