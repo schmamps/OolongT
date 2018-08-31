d40b696f180d355c2aee44b89f2f5f236fa08281
Andrew Champion <awchampion@gmail.com>
2018-08-31 13:58:24 -0700

    add NONE_STR type hint

--- src/oolongt/constants.py
+++ src/oolongt/constants.py
@@ -1,4 +1,4 @@
-from typing import List  # noqa
+import typing
 from pathlib import Path
 
 # summarizer
@@ -14,7 +14,10 @@ DEFAULT_IDIOM = 'default'
 DEFAULT_IDEAL_LENGTH = 20
 DEFAULT_LANGUAGE = 'english'
 DEFAULT_NLTK_STOPS = True
-DEFAULT_USER_STOPS = []   # type: List[str]
+DEFAULT_USER_STOPS = []   # type: typing.List[str]
 
 # scored keyword
 KEYWORD_SCORE_K = 1.5
+
+# types
+NONE_STR = typing.Union[str, None]
