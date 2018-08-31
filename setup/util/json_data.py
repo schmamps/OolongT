eead8145230f38e46d0f6cd4d09237145c9fdce4
Andrew Champion <awchampion@gmail.com>
2018-08-31 14:13:42 -0700

    add docstrings

--- setup/util/json_data.py
+++ setup/util/json_data.py
@@ -1,3 +1,4 @@
+"""JSON data utility"""
 import typing
 from collections import OrderedDict
 from io import open as io_open
@@ -10,10 +11,26 @@ from src.oolongt.pipe import pipe
 
 
 def noop(val: typing.Any) -> typing.Any:
-    return noop
+    """Return input value
+
+    Arguments:
+        val {typing.Any} -- any value
+
+    Returns:
+        typing.Any -- input value
+    """
+    return val
 
 
 def read(path: Path) -> OrderedDict:
+    """Get contents of JSON at `path`
+
+    Arguments:
+        path {Path} -- path to file
+
+    Returns:
+        OrderedDict -- JSON data
+    """
     with io_open(str(path), 'r', encoding='utf-8') as fp:
         data = json_load(fp, object_pairs_hook=OrderedDict)
 
@@ -21,6 +38,14 @@ def read(path: Path) -> OrderedDict:
 
 
 def dump_data(data: typing.Dict) -> str:
+    """Convert dictionary to JSON string
+
+    Arguments:
+        data {typing.Dict} -- data dictionary
+
+    Returns:
+        str -- JSON string
+    """
     return dumps(data, indent='\t')
 
 
@@ -28,8 +53,17 @@ def write(
         data: typing.Union[typing.Dict, OrderedDict],
         path: Path,
         pre_proc: PROC_TYPE = noop,
-        post_proc: PROC_TYPE = noop
-        ) -> None:
+        post_proc: PROC_TYPE = noop):
+    """Write `data` to `path` with optional pre- and post-processsors
+
+    Arguments:
+        data {typing.Union[typing.Dict, OrderedDict]} -- data dictionary
+        path {Path} -- path to write
+
+    Keyword Arguments:
+        pre_proc {PROC_TYPE} -- process `data` dict w/ func (default: {noop})
+        post_proc {PROC_TYPE} -- process JSON string w/ func (default: {noop})
+    """
     makedirs(str(path.parent), exist_ok=True)
     json_str = pipe(data, pre_proc, dump_data, post_proc)  # type: str
 
