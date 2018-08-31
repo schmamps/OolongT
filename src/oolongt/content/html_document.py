e5bfa99efd3040ce062de05b86b7c45b80b03b74
Andrew Champion <awchampion@gmail.com>
2018-08-31 16:08:09 -0700

    doc and lint

--- src/oolongt/content/html_document.py
+++ src/oolongt/content/html_document.py
@@ -1,17 +1,22 @@
-import typing
-
+"""Parse HTML documents"""
 from bs4.element import Tag
 
+from ..typedefs import NONE_STR
+from ..typedefs.ugly_query import UglyQuery
+from ..ugly_soup import UglySoup
 from .text_document import TextDocument
-from .ugly_soup import UglySoup as BeautifulSoup
-from .ugly_query import UglyQuery
 
 
-QUERY_RESULT = typing.Union[str, None]
+def process(html: str) -> UglySoup:
+    """Parse and filter `html`
 
+    Arguments:
+        html {str} -- document HTML
 
-def process(html: str) -> BeautifulSoup:
-    soup = BeautifulSoup(html, features='html.parser')
+    Returns:
+        UglySoup -- BeautifulSoup subclass
+    """
+    soup = UglySoup(html)
     ignore_tags = [
         'script',
         'noscript',
@@ -24,15 +29,14 @@ def process(html: str) -> BeautifulSoup:
     return soup
 
 
-def get_body(src: BeautifulSoup) -> str:
+def get_body(src: UglySoup) -> str:
     """Get document body content
 
     Arguments:
-        src {BeautifulSoup} -- [description]
-        path {str} -- [description]
+        src {UglySoup} -- BeautifulSoup
 
     Returns:
-        str -- [description]
+        str -- best match for content
     """
     queries = (
         UglyQuery('main'),
@@ -43,14 +47,14 @@ def get_body(src: BeautifulSoup) -> str:
     return body
 
 
-def get_og_title(tag: Tag) -> QUERY_RESULT:
-    """Get OpenGraph-based title
+def get_og_title(tag: Tag) -> NONE_STR:
+    """Get OpenGraph-based title if any, else None
 
     Arguments:
         tag {Tag} -- page element
 
     Returns:
-        QUERY_DATA -- str or None
+        NONE_STR -- str or None
     """
     if tag.get('property') == 'og:title':
         return tag.get('content')
@@ -58,7 +62,15 @@ def get_og_title(tag: Tag) -> QUERY_RESULT:
     return None
 
 
-def get_title(src: BeautifulSoup) -> str:
+def get_title(src: UglySoup) -> str:
+    """Get title tag string, else empty
+
+    Arguments:
+        src {UglySoup} -- BeautifulSoup
+
+    Returns:
+        str -- title (if any)
+    """
     queries = (
         UglyQuery('meta', get_og_title),
         UglyQuery('title'), )
@@ -67,18 +79,28 @@ def get_title(src: BeautifulSoup) -> str:
     return title
 
 
-def get_keywords(src: BeautifulSoup) -> typing.List[str]:
-    return []
-
-
 class HtmlDocument(TextDocument):
-    def get_source(self, path: str):
+    """Parse HTML"""
+    def get_source(self, path: str) -> UglySoup:
+        """Load HTML from `path`
+
+        Arguments:
+            path {str} -- local/remote path to HTML
+
+        Returns:
+            UglySoup -- BeautifulSoup
+        """
         html = super().get_source(path)
         src = process(html)
 
         return src
 
     def __init__(self, path: str) -> None:
+        """Initialize
+
+        Arguments:
+            path {str} -- path to HTML
+        """
         src = self.get_source(path)
 
         body = get_body(src)
@@ -88,5 +110,16 @@ class HtmlDocument(TextDocument):
         super().__init__(body, title, keywords, path)
 
     @staticmethod
-    def supports(path: str, ext: str) -> bool:
-        return path.startswith('http') or ext.startswith('htm')
+    def supports(path: NONE_STR, ext: NONE_STR) -> bool:
+        """Register support for given extension (path ignored)
+
+        Arguments:
+            path {str} -- path to document
+            ext {str} -- extension of document
+
+        Returns:
+            bool -- format is supported
+        """
+        path_str = str(path)[:4]
+
+        return path_str in ['http', 'ftp:'] or str(ext).startswith('htm')
