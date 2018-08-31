394f2815a66166da4539bec0118b3b6d7f14cf83
Andrew Champion <awchampion@gmail.com>
2018-08-31 15:55:28 -0700

    doc and lint

--- src/oolongt/content/docx_document.py
+++ src/oolongt/content/docx_document.py
@@ -5,10 +5,24 @@ from .binary_document import BinaryDocument
 
 
 class DocxDocument(BinaryDocument):
+    """Parse Word XML"""
     def get_source(self, path: str) -> DocxFile:
+        """Get docx2txt object for file at `path`
+
+        Arguments:
+            path {str} -- path to document
+
+        Returns:
+            DocxFile -- docx2txt
+        """
         return DocxFile(path)
 
     def __init__(self, path: str) -> None:
+        """Initialize for file at `path`
+
+        Arguments:
+            path {str} -- [description]
+        """
         src = self.get_source(path)
 
         body = self.get_body(src.main)
@@ -19,4 +33,13 @@ class DocxDocument(BinaryDocument):
 
     @staticmethod
     def supports(_: str, ext: str) -> bool:
+        """Register support for given extension (path ignored)
+
+        Arguments:
+            _ {str} -- ignored
+            ext {str} -- extension of document
+
+        Returns:
+            bool -- format is supported
+        """
         return ext == 'docx'
