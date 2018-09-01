02aa9c76fa9225fa9f8cd6e5617c3ea3387c386a
Andrew Champion <awchampion@gmail.com>
2018-09-01 10:26:47 -0700

    consistency: `src` variable name fallback for keyword/title failures in PyPDF correctly handle open PDF

--- src/oolongt/content/pdf_document.py
+++ src/oolongt/content/pdf_document.py
@@ -1,61 +1,99 @@
 """Content extractor for PDF files"""
 from PyPDF2 import PdfFileReader
+from PyPDF2.pdf import DocumentInformation
 
 from .binary_document import BinaryDocument
 
 
-def get_page(doc: PdfFileReader, page_num: int) -> str:
+def get_page(src: PdfFileReader, page_num: int) -> str:
     """Get page of content from PDF document
 
     Arguments:
-        doc {PdfFileReader} -- PDF document
+        src {PdfFileReader} -- PDF document
         page_num {int} -- page number
 
     Returns:
         str -- text of an individual page
     """
-    page = doc.getPage(page_num)
+    page = src.getPage(page_num)
 
     return page.extractText()
 
 
-def get_body(doc: PdfFileReader) -> str:
+def get_body(src: PdfFileReader) -> str:
     """Get body of PDF document
 
     Arguments:
-        doc {PdfFileReader} -- PDF document
+        src {PdfFileReader} -- PDF document
 
     Returns:
         str -- document body property
     """
-    num_pages = doc.getNumPages()
-    pages = [get_page(doc, i) for i in range(num_pages)]
+    num_pages = src.getNumPages()
+    pages = [get_page(src, i) for i in range(num_pages)]
 
     return '\n'.join(pages)
 
 
-def get_title(doc: PdfFileReader) -> str:
+def get_title(info: DocumentInformation) -> str:
     """Get title (if any)
 
     Arguments:
-        doc {PdfFileReader} -- PDF document
+        info {DocumentInformation} -- PDF document info
 
     Returns:
         str -- document title property
     """
-    info = doc.getDocumentInfo()
+    try:
+        title = info.title
 
-    return info.title
+        if title is not None:
+            return title
+
+    except AttributeError:
+        pass
+
+    try:
+        return info['/Title']
+
+    except KeyError:
+        pass
+
+    return ''
+
+
+def get_keywords(info: DocumentInformation) -> str:
+    """Get keywords (if any)
+
+    Arguments:
+        info {DocumentInformation} -- PDF document info
+
+    Returns:
+        str -- document keywords property
+    """
+    try:
+        return info.keywords
+    except AttributeError:
+        pass
+
+    try:
+        return info['/Keywords']
+    except KeyError:
+        pass
+
+    return ''
 
 
 class PdfDocument(BinaryDocument):
+    """Parse PDF"""
     def __init__(self, path: str) -> None:
-        stream = self.get_stream(path)
-        doc = PdfFileReader(stream)
+        with self.get_stream(path) as stream:
+            src = PdfFileReader(stream)
+            info = src.getDocumentInfo()
 
-        body = get_body(doc)
-        title = get_title(doc)
-        keywords = self.get_keywords(doc)  # TODO: implement get_keywords
+            body = get_body(src)
+            title = get_title(info)
+            keywords = get_keywords(info)
 
         super().__init__(body, title, keywords, path)
 
