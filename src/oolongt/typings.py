60dc35581d2c1d28f9c9776895a32c2eb4372239
Andrew Champion <awchampion@gmail.com>
2018-09-11 15:31:19 -0700

    add AnyList

--- src/oolongt/typings.py
+++ src/oolongt/typings.py
@@ -5,6 +5,7 @@ import typing  # noqa: F401
 PathOrString = typing.Union[str, pathlib.Path]
 OptionalString = typing.Optional[str]
 StringList = typing.List[str]
+AnyList = typing.List[typing.Any]
 OptionalStringList = typing.Union[OptionalString, StringList]
 DictOfAny = typing.Dict[str, typing.Any]
 DictOfFloat = typing.Dict[str, float]
