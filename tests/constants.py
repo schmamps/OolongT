86452c0dafad4b12942c29c44ad46bcc33e61c34
Andrew Champion <awchampion@gmail.com>
2018-09-11 16:41:10 -0700

    update for relocation in 0be7e1193

--- tests/constants.py
+++ tests/constants.py
@@ -2,6 +2,6 @@ from pathlib import Path
 
 DATA_PATH = Path(__file__).parent.joinpath('data')
 TEXT_PATH = DATA_PATH.joinpath('text')
-DOC_PATH = DATA_PATH.joinpath('docs')
+DOC_PATH = DATA_PATH.joinpath('content')
 IDIOM_PATH = DATA_PATH.joinpath('idioms')
 SAMPLES = ['cambodia', 'cameroon', 'canada']
