eecf63f83063383322ceab409753972c90c05f6f
Andrew Champion <awchampion@gmail.com>
2018-09-17 18:06:32 -0700

    lots

--- tests/params/string.py
+++ tests/params/string.py
@@ -1,3 +1,4 @@
+"""String module test parameters"""
 from .helpers import parametrize
 
 LIST_TEST_INT = list(range(1, 6))
@@ -9,19 +10,33 @@ ALPHA_SIMPLE = 'abcdefghijklmnopqrstuvwxyz'
 ALPHA_COMPLEX = 'åbçdéfghîjklmnöpqrštùvwxyz'
 
 
-def pad(val: str, spaces: int=2):
+def pad(val: str, spaces: int = 2) -> str:
+    """Pad val on left and right with `spaces` whitespace chars
+
+    Arguments:
+        val {str} -- string to pad
+
+    Keyword Arguments:
+        spaces {int} -- number of spaces to pad on either side (default: {2})
+
+    Returns:
+        str -- padded string
+    """
     pad_str = ' ' * spaces
+
     return '{0}{1}{0}'.format(pad_str, val)
 
 
-def param_cast_list():
-    """Parametrize `test_cast_list`"""
+def param_cast():
+    """Parametrize `test_cast`"""
     names = 'val,expected'
     vals = (
+        (1, '1', ),
+        ('a', 'a'),
         (LIST_TEST_INT, LIST_TEST_STR),
         ('12345', LIST_TEST_STR),  # sorry!
     )
-    ids = ('int', 'oops', )
+    ids = ('solo-int', 'solo-str', 'list-int', 'oops', )
 
     return parametrize(names, vals, ids)
 
@@ -38,28 +53,18 @@ def param_define_split_join():
     return parametrize(names, vals, ids)
 
 
-def param_strip_str():
-    """Parametrize `test_strip_str`"""
+def param_strip():
+    """Parametrize `test_strip`"""
     names = 'val,expected'
     vals = (
         (STR_TEST_SPACES, STR_TEST_SPACES),
         ('    {}'.format(STR_TEST_SPACES), STR_TEST_SPACES),
         ('{}    '.format(STR_TEST_COMMAS), STR_TEST_COMMAS),
         ('  {}  '.format(STR_TEST_CSV), STR_TEST_CSV),
-    )
-    ids = ('none', 'left', 'right', 'both', )
-
-    return parametrize(names, vals, ids)
-
-
-def param_strip_strs():
-    """Parametrize `test_strip_strs`"""
-    names = 'val,expected'
-    vals = (
         (LIST_TEST_STR, LIST_TEST_STR),
         ([pad(num) for num in LIST_TEST_STR], LIST_TEST_STR),
     )
-    ids = ('none', 'both', )
+    ids = ('1-none', '1-left', '1-right', '1-center', '[]-none', '[]-both', )
 
     return parametrize(names, vals, ids)
 
@@ -78,12 +83,59 @@ def param_filter_empty():
 
 def param_split():
     """Parametrize `test_split`"""
-    names = 'kwargs,expected'
-    vals = (({}, []), )
-    ids = ('ayy', )
-
-    vals = [({}, None), ({}, None), ]
-    ids = ['ayy', 'lmao']
+    names = 'val,kwargs,expected'
+    vals = (
+        (
+            LIST_TEST_INT,
+            {},
+            LIST_TEST_STR,
+        ),
+        (
+            '|'.join(LIST_TEST_STR),
+            {},
+            ['|'.join(LIST_TEST_STR)],
+        ),
+        (
+            '|'.join(LIST_TEST_STR),
+            {'sep': r'\|'},
+            LIST_TEST_STR,
+        ),
+        (
+            ' 1| 2 | 3  |  4  |  5   ',
+            {'sep': r'\|', },
+            LIST_TEST_STR,
+        ),
+        (
+            ' 1| 2 | 3  |  4  |  5   ',
+            {'sep': r'\|', 'strip_text': False, },
+            [' 1', ' 2 ', ' 3  ', '  4  ', '  5   '],
+        ),
+        (
+            ' 1|| 2 | | 3  |  |  4  |   |  5   ',
+            {'sep': r'\|', },
+            LIST_TEST_STR,
+        ),
+        (
+            ' 1|| 2 | | 3  |  |  4  |   |  5   ',
+            {'sep': r'\|', 'allow_empty': True},
+            ['1', '', '2', '', '3', '', '4', '', '5'],
+        ),
+        (
+            ' 1|| 2 | | 3  |  |  4  |   |  5   ',
+            {'sep': r'\|', 'strip_text': False, 'allow_empty': True},
+            [' 1', '', ' 2 ', ' ', ' 3  ', '  ', '  4  ', '   ', '  5   '],
+        ),
+    )
+    ids = (
+        'defaults',
+        'sep_default',
+        'sep_custom',
+        'strip_default',
+        'strip_disabled',
+        'allow_default',
+        'allow_enabled',
+        'all_options',
+    )
 
     return parametrize(names, vals, ids)
 
