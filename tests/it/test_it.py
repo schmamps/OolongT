ab6d8e949dad47929d5b79d9624dd9ca10ae9cb3
Andrew Champion <awchampion@gmail.com>
2018-09-17 18:05:29 -0700

    docstrings

--- tests/it/test_it.py
+++ tests/it/test_it.py
@@ -1,3 +1,4 @@
+"""Test it subpackage"""
 import typing
 
 from src.oolongt import it
@@ -6,6 +7,12 @@ from tests.params.it import param_it_erable, param_it_erate
 
 @param_it_erable()
 def test_it_erable(val: typing.Any, expected: bool):
+    """Test `it.erable`
+
+    Arguments:
+        val {typing.Any} -- any value
+        expected {bool} -- value is iterable (but not str)
+    """
     received = it.erable(val)
 
     assert received == expected
@@ -13,6 +20,11 @@ def test_it_erable(val: typing.Any, expected: bool):
 
 @param_it_erate()
 def test_it_erate(val: typing.Any):
+    """Test `it.erate`
+
+    Arguments:
+        val {typing.Any} -- any value
+    """
     received = it.erate(val)
 
     assert it.erable(received)
